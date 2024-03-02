96ea2259b9e87d458467d5bc47d294b29e01f78d
Sagar Gidwani <86357062+SagarGidwani@users.noreply.github.com>
2024-03-02 07:30:51 +0530

    Update backend.tf

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "ajay-mrcloudbook777" # Replace with your actual S3 bucket name
+    bucket = "tf-sagar-demo"
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
